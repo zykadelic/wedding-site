43c2b94e39f1c4eded08b3ebc1c5f88ad248ffa3
Andreas Fransson <andreas@enigma.cc>
2013-11-23 03:38:53 +0100

    adds raw html version of all public pages

---
+++ app/controllers/public_controller.rb
@@ -0,0 +1,14 @@
+class PublicController < ApplicationController
+
+	def index;end
+
+	def wedding;end
+
+	def living;end
+
+	def navigation;end
+
+	def tips;end
+
+	def rsvp;end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,18 @@ Wedding::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'public#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
+  get 'wedding'     => 'public#wedding'
+  get 'living'      => 'public#living'
+  get 'navigation'  => 'public#navigation'
+  get 'tips'        => 'public#tips'
+  get 'rsvp'        => 'public#rsvp'
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
