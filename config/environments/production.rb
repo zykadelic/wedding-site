bf0dee22f39678d6fe1fb9228839828fa5818952
Andreas Fransson <andreas@enigma.cc>
2013-11-23 05:44:39 +0100

    adds responsive html5 boilerplate

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ Wedding::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w(shared/modernizr-respond-1.1.0.min)
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
