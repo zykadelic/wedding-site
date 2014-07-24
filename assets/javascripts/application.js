e73ee6e9ebb502d43d09371c59eb8cffe1fa688c
Andreas Fransson <andreas@enigma.cc>
2014-07-25 01:12:56 +0200

    adds hover for touchscreens

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -1,5 +1,9 @@
 $(function(){
 	if($('#wedding-day').length){
 		$.lightbox();
+
+		$(document).on('touchstart', '#picture-lightbox .picture', function(e){
+			$(this).toggleClass('touch-hover');
+		});
 	}
 });
